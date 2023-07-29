809fc292d87f081cf6830a5e312f70d4aa48e580
不出我所廖 <liaomouyo@163.com>
2023-07-29 21:37:50 +0800

    初始化DDD工程

---
+++ chat-api-interfaces/src/main/java/com/liao/chat/api/application.java
@@ -0,0 +1,17 @@
+package com.liao.chat.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Liao YunYang
+ * @description 启动入口
+ * @github https://github.com/BuChuWoSuoLiao
+ * @CreateTime 2023-07-29  21:17
+ */
+@SpringBootApplication
+public class application {
+    public static void main(String[] args) {
+        SpringApplication.run(application.class, args);
+    }
+}
---
+++ chat-api-interfaces/src/test/java/com/liao/chat/api/test/ApiTest.java
@@ -0,0 +1,13 @@
+package com.liao.chat.api.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Liao YunYang
+ * @description 单元测试
+ * @github https://github.com/BuChuWoSuoLiao
+ * @CreateTime 2023-07-29  21:31
+ */
+@SpringBootTest
+public class ApiTest {
+}
